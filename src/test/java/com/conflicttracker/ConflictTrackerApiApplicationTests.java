675ed6f2c898f6e78f309019793136c76cd41e85
Tuto Mateo <tutoterrero7@gmail.com>
2025-12-02 19:09:32 +0100

    Initial commit

---
+++ src/test/java/com/conflicttracker/ConflictTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conflicttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConflictTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
